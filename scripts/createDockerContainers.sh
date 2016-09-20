fbc47593c3f82b148130514d5e65366d815b8695
rkaliyaperumal <r.kaliyaperumal@lumc.nl>
2016-09-20 16:02:18 +0200

    first commit

--- scripts/createDockerContainers.sh
+++ scripts/createDockerContainers.sh
@@ -4,3 +4,5 @@ echo "Creating fdp docker container"
 sudo docker run --name fdp -p 8080:8080 -d biosemantics/fdp
 echo "Creating virtuoso docker container"
 sudo docker run --name virtuoso -p 8890:8890 -p 1111:1111 -e DBA_PASSWORD=dba -e SPARQL_UPDATE=true -d tenforce/virtuoso
+echo "Creating linked-data-fragment server docker container"
+sudo docker run --name ldf-server -p 5001:5001 -d biosemantics/ldf-server-turtle-based
--- scripts/startDockerContainers.sh
+++ scripts/startDockerContainers.sh
@@ -1,12 +1,16 @@
-echo "Start openrefine docker container"
+echo "Starting openrefine docker container"
 sudo docker start openrefine
 
-echo "Start fdp docker container"
+echo "Starting fdp docker container"
 sudo docker start fdp
 
-echo "Start virtuoso docker container"
+echo "Starting virtuoso docker container"
 sudo docker start virtuoso
+
+echo "Starting ldf server docker container"
+sudo docker start ldf-server 
 
 echo "FDP available at 'http://localhost:8080/fdp/swagger-ui.html'"
 echo "Openrefine available at 'http://localhost:3333'"
 echo "Virtuoso available at 'http://localhost:8890/sparql'"
+echo "Linked data fragment server available at 'http://localhost:5001'"
